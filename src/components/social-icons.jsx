b3e8ca04a7d87ebc36d572335d97f9806eaaefb8
camille villard <hello@camillevillard.com>
2019-05-30 19:18:23 -0400

    landing page

--- src/components/social-icons.jsx
+++ src/components/social-icons.jsx
@@ -11,19 +11,19 @@ const SocialIcons = (props) => {
   return(
     <div id={props.id}>
       <div id="instagram" className="social-icon-white">
-      <a href="https://instagram.com/plaatine" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/alexandralarouche/" target="_blank" rel="noopener noreferrer">
         <FaInstagram />
       </a>
       </div>
 
-      <div id="twitter" className="social-icon-white">
-      <a href="https://twitter.com/Plaatine" target="_blank" rel="noopener noreferrer">
+      <div id="youtube" className="social-icon-white">
+      <a href="https://www.youtube.com/channel/UCUCkH561i3VjDQPJrGdGFQQ" target="_blank" rel="noopener noreferrer">
         <FaYoutube />
         </a>
       </div>
 
       <div id="facebook" className="social-icon-white">
-      <a href="https://www.facebook.com/Platine-413064029464479/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/alexfashionbeauty1" target="_blank" rel="noopener noreferrer">
         <FaFacebookF />
         </a>
       </div>
