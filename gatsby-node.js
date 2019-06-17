c109f2ccc77a36940b883890c81f93a8157d5fde
camille villard <hello@camillevillard.com>
2019-06-17 16:06:26 -0400

    wp stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,48 +45,48 @@ exports.createPages = ({ graphql, actions }) => {
 
 
   // starting the create page actions
-  const createWpPages = new Promise((resolve, reject) => {
-    const query = graphql(`
-      {
-        allWordpressPage {
-          edges {
-            node {
-              id
-              slug
-            }
-          }
-        }
-      }
-    `)
+  // const createWpPages = new Promise((resolve, reject) => {
+  //   const query = graphql(`
+  //     {
+  //       allWordpressPage {
+  //         edges {
+  //           node {
+  //             id
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
 
-    query.then(result => {
-      if (result.errors) {
-        console.error(results.errors)
-        reject(result.error)
-      }
+  //   query.then(result => {
+  //     if (result.errors) {
+  //       console.error(results.errors)
+  //       reject(result.error)
+  //     }
 
-      // grab the content pulled thanks to the graphql query
-      const pageEdges = result.data.allWordpressPage.edges
+  //     // grab the content pulled thanks to the graphql query
+  //     const pageEdges = result.data.allWordpressPage.edges
 
-      // create a new static page for each one of the articles found
-      pageEdges.forEach(edge => {
-        createPage({
-          path: `/${edge.node.slug}`,
-          component: path.resolve(`./src/templates/page.jsx`),
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          context: {
-            id: edge.node.id,
-          },
-        })
-      })
-      resolve()
-    }) // end of query.then
-  }) // end of createWpPosts
+  //     // create a new static page for each one of the articles found
+  //     pageEdges.forEach(edge => {
+  //       createPage({
+  //         path: `/${edge.node.slug}`,
+  //         component: path.resolve(`./src/templates/page.jsx`),
+  //         // Data passed to context is available
+  //         // in page queries as GraphQL variables.
+  //         context: {
+  //           id: edge.node.id,
+  //         },
+  //       })
+  //     })
+  //     resolve()
+  //   }) // end of query.then
+  // }) // end of createWpPosts
 
 
 
-  return Promise.all([createWpPosts, createWpPages])
+  return Promise.all([createWpPosts])
 } // createPages
 
 
