b44c46557be681c8fee16020b955b393e58de7f5
camille villard <hello@camillevillard.com>
2019-06-27 17:57:59 -0400

    styling tabs

--- src/pages/shop-look.jsx
+++ src/pages/shop-look.jsx
@@ -15,14 +15,17 @@ const FavorisCard = ({favori}) => {
   return(
     <div className="favoris-card">
 
+    <a href={`${favori.acf.url_du_produit}`} target="_blank" rel="noopener noreferrer">
+
       <img src={favori.featured_media.source_url} alt={favori.title}/>
 
       <div className="favoris-card-content">
-        <h3 className="favoris-card-title">
-          <a href={`${favori.acf.url_du_produit}`} target="_blank" rel="noopener noreferrer">{favori.title}</a>
-        </h3>
+        <h3 className="favoris-card-title">{favori.title}</h3>
         <p>{favori.acf.nom_marque}</p>
       </div>
+
+    </a>
+
 
     </div>
   )
@@ -40,6 +43,8 @@ class ShopLook extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      // by default when arriving on the page
+      // we want to see all favorites
       activeTab: 'all'
     }
   }
@@ -48,18 +53,24 @@ class ShopLook extends React.Component {
     // we use data attributes to target the catehpry we want to show
     const allFavoris = this.props.data.allWordpressWpFavoris.edges
     const categoryToShow =  e.target.dataset.category
+    // if see all is targeted, we need to set manually the state
     if (categoryToShow === "tout") {
       this.setState({ activeTab: 'all'})
     } else {
+      // else, grab the category of the list-item clicked
       this.setState({ activeTab: categoryToShow})
     }
   }
 
 
   filterByCategories = (cards, category) => {
+    // again, we need to handle the « see all » possibily
+    // we return all the cards if activeTab wanted is all
     if (category === "all") {
       return cards
     } else {
+      // we filter them if not, using the category name
+      // matching the data attributes
       const filteredCards = cards.filter( card => {
         return card.node.categories[0].name === category
       })
@@ -78,25 +89,20 @@ class ShopLook extends React.Component {
 
         <div className="favoris-filter-tabs">
 
-        <ul className="list-inline">
-          <li key='0' data-category="tout" onClick={this.handleClick}>voir tout</li>
-          {allCategory.map( cat => {
-            return <li key={cat.node.id} data-category={cat.node.name} onClick={this.handleClick}>
-                        {cat.node.name}</li>
-          })}
-        </ul>
-
-erdfg
+          <ul className="list-inline">
+            <li key='0' data-category="tout" onClick={this.handleClick}>voir tout</li>
+            {allCategory.map( cat => {
+              return <li key={cat.node.id} data-category={cat.node.name} onClick={this.handleClick}>
+                          {cat.node.name}</li>
+            })}
+          </ul>
 
         </div>
 
 
         <div className="favoris-container">
-
-        {this.filterByCategories(allFavoris, this.state.activeTab).map( fav => <FavorisCard favori={fav.node} key={fav.node.id} /> )}
-
-
-
+          {this.filterByCategories(allFavoris, this.state.activeTab)
+            .map( fav => <FavorisCard favori={fav.node} key={fav.node.id} /> )}
         </div>
 
 
