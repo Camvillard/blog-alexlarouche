16d6d633dcaf7fa044dd6fbc1401acfe546c2875
camille villard <hello@camillevillard.com>
2019-08-14 21:02:08 -0400

    contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,13 +10,56 @@ import meta from "../data/meta";
 
 // styles & assets
 
-const ContactPage = () => (
-  <Layout>
-    <SEO title="Contact" keywords={meta.seo.keywords} />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    }
+  }
+
+  handleBlur = (e) => {
+    console.log(e.target.name)
+    // this.setState({
+    //   [e.target.name]:  e.target.value
+    // })
+  }
+
+  render() {
+
+    return(
+      <div id="contact-page">
+
+        <h1 className="main-page-title">contact</h1>
+
+        <form className="form-white">
+          <div className="form-element">
+            <input type="text" name="name" onBlur={this.handleBlur} className="form-name"/>
+          </div>
+
+          <div className="form-element">
+            <input type="email" name="email" onBlur={this.handleBlur}/>
+          </div>
+
+          <div className="form-element">
+            <input type="email" name="subject" onBlur={this.handleBlur}/>
+          </div>
+
+          <div className="form-element">
+            <textarea name="message" id="" cols="30" rows="10"></textarea>
+          </div>
+
+        </form>
+
+      </div>
+
+
+    )
+  }
+}
+
 
 export default ContactPage
