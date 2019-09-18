e4679cde2f866468724bae82d87be09d13e755dc
camille villard <hello@camillevillard.com>
2019-09-18 13:38:09 -0400

    changed  version for wp

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -165,7 +165,23 @@ export const query = graphql`
       }
     }
 
-
+    allWordpressWpComments(filter: {post: {eq: $postId}}){
+      totalCount
+      edges {
+        node {
+          id
+          post
+          content
+          author
+          author_name
+          author_url
+          date
+          author_avatar_urls {
+            wordpress_96
+          }
+        }
+      }
+    }
   }
 `
 
